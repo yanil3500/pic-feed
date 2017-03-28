683b20ce557e583c6f3c08b97be1ca2e59dd8b9f
yanil3500 <yanil3500@gmail.com>
2017-03-28 11:08:09 -0700

    Adds functionality that enables posting and retrieving CKRecords to/from iCloud; Add extensions that takes the data from the CKRecord and processes data to UIImages.

--- PicFeed/PicFeed/CloudKit.swift
+++ PicFeed/PicFeed/CloudKit.swift
@@ -9,7 +9,10 @@
 import CloudKit
 import Foundation
 
+
+typealias PostCompletion = (Bool) -> ()
 class CloudKit {
+
     
     static let shared = CloudKit()
     
@@ -20,5 +23,33 @@ class CloudKit {
     
     var privateDB : CKDatabase {
         return self.container.privateCloudDatabase
+    }
+    
+    func save(post: Post, completion: @escaping PostCompletion){
+        do {
+            if let record = try Post.recordFor(post: post) {
+                
+                privateDB.save(record, completionHandler: { (record, error) in
+                    
+                    //Means saving to privateDb was unsuccessful
+                    if error != nil {
+                        completion(false)
+                        //The return statement will prevent the execution from falling through to the bottom of the conditional to the rest of the statements in the codeblock
+                        return
+                    }
+                    
+                    //If there is a record, it means a record was posted to iCloud
+                    if let record = record {
+                        print(record)
+                        completion(true)
+                    } else {
+                    //If there isn't a record and there isn't an error
+                        completion(false)
+                    }
+                })
+            }
+        } catch {
+            print(error)
+        }
     }
 }
--- PicFeed/PicFeed/Post.swift
+++ PicFeed/PicFeed/Post.swift
@@ -7,13 +7,54 @@
 //
 
 import UIKit
-
+import CloudKit
 //Posting images to iCloud
+//Images first have to be converted to raw data and written to the disk
+//The data that is written to disk is what is transmitted to iCloud
+
 
 class Post {
     let image : UIImage
     
     init(image: UIImage) {
         self.image = image
+    }
+}
+
+enum PostError : Error {
+    //If there is an error taking to UIImage to rawData
+    case WritingImageToData
+    case writingDataToDisk
+}
+
+extension Post {
+    
+    //Retrieves the CKRecord for a given Post
+    class func recordFor(post: Post) throws -> CKRecord? {
+        
+        //the first parameter is the image that is to be converted, the second parameter is the compression setting for the image
+        guard let data = UIImageJPEGRepresentation(post.image, 0.7) else {
+            //If image to data conversion fail, throw PostError.WritingImageToData
+            throw PostError.WritingImageToData
+        }
+        
+        do {
+            try data.write(to: post.image.path)
+            //Create an asset; Stores raw data (image, video, audio) into a CKAsset
+            
+            //Retrieves the image stored in the directory specified by the path
+            let asset = CKAsset(fileURL: post.image.path)
+            
+            //Tells CloudKit to create a new record of type 'Post' in the cloud
+            let record = CKRecord(recordType: "Post")
+            record.setValue(asset, forKey: "image")
+            
+            
+            return record
+        } catch {
+            throw PostError.writingDataToDisk
+        }
+        
+        return nil
     }
 }

2dafbd867c7df4c5258ecd62b90d2e072ad70311
yanil3500 <yanil3500@gmail.com>
2017-03-28 10:15:10 -0700

    Creates a Post class in model; Sends images to iCloud

---
+++ PicFeed/PicFeed/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  PicFeed
+//
+//  Created by Elyanil Liranzo Castro on 3/28/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import UIKit
+
+//Posting images to iCloud
+
+class Post {
+    let image : UIImage
+    
+    init(image: UIImage) {
+        self.image = image
+    }
+}

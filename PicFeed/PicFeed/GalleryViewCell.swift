2f29886f9ea64c5f7c5f86199517bf37362caba4
yanil3500 <yanil3500@gmail.com>
2017-03-30 18:30:55 -0700

    Lab

--- PicFeed/PicFeed/GalleryViewCell.swift
+++ PicFeed/PicFeed/GalleryViewCell.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class GalleryViewCell: UICollectionViewCell {
-    
-   
+
     @IBOutlet weak var imageView: UIImageView!
-    
+
     @IBOutlet weak var dateCreated: UILabel!
     var datePosted: String = ""
     var post: Post! {
@@ -21,15 +20,14 @@ class GalleryViewCell: UICollectionViewCell {
             self.datePosted = Post.getDate(dateTo: post.dateCreated!)
         }
     }
-    
+
     //Accounts for bugs in reusing cells (Ex: Retrieving images asynchronously; reuse the cell without clear, resulting in the new image beign stack on top of the old image)
     override func prepareForReuse() {
-        
+
         super.prepareForReuse()
-        
+
         self.imageView.image = nil
         self.datePosted = ""
     }
-    
-    
+
 }
--- PicFeed/PicFeed/GalleryViewController.swift
+++ PicFeed/PicFeed/GalleryViewController.swift
@@ -8,55 +8,95 @@
 
 import UIKit
 
+protocol GalleryViewControllerDelegate : class {
+    func galleryController(didSelect image: UIImage)
+}
+
 class GalleryViewController: UIViewController {
     
-    @IBOutlet weak var galleryViewController: UICollectionView!
+    weak var delegate : GalleryViewControllerDelegate?
     
+
+    @IBOutlet weak var galleryViewController: UICollectionView!
+
     var allPosts = [Post]() {
         didSet {
             self.galleryViewController.reloadData()
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.galleryViewController.delegate = self
         self.galleryViewController.dataSource = self
         self.galleryViewController.collectionViewLayout = GalleryCollectionViewLayoutController(columns: 2)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         update()
     }
-    
-    func update(){
+
+    func update() {
         CloudKit.shared.getPosts { (posts) in
             guard let posts = posts else { fatalError("Failed to get posts.") }
             self.allPosts = posts
         }
     }
-}
-
-//MARK: UICollectionViewDataSource Extension
-extension GalleryViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryViewCell.identifier, for: indexPath) as! GalleryViewCell
+    
+    @IBAction func userPinched(_ sender: UIPinchGestureRecognizer) {
+        //collectionView.collectionViewLayout as? GalleryCollectionLayout else {return}
         
-        cell.post = self.allPosts[indexPath.row]
+        guard let layout = galleryViewController.collectionViewLayout as? GalleryCollectionViewLayoutController else { return }
+        switch sender.state {
+        case .began:
+            print("User pinched.")
+        case .changed:
+            print("<---User pinch changed--------->")
+        case .ended:
+            print("Pinch ended.")
+            let columns = sender.velocity > 0 ? layout.columns - 1 : layout.columns + 1
+            
+            if columns < 1 || columns > 10 { return }
+            
+            galleryViewController.setCollectionViewLayout(GalleryCollectionViewLayoutController(columns: columns), animated: true)
+            
+        default:
+            print("Unknown sender state")
+        }
         
-        var title = UILabel(frame: CGRect(x: 0, y: 0, width: cell.bounds.size.width, height: 40))
-        
-        title.backgroundColor = UIColor.white
-        title.text = "Posted: \(cell.datePosted)"
-        title.numberOfLines = 0
-        cell.contentView.addSubview(title)
-        
-        return cell
     }
     
+    
+}
+
+
+
+
+
+// MARK: UICollectionViewDataSource Extension
+extension GalleryViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryViewCell.identifier, for: indexPath) as! GalleryViewCell
+
+        cell.post = self.allPosts[indexPath.row]
+
+        cell.dateCreated.text = cell.datePosted
+
+        return cell
+    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return allPosts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let delegate = self.delegate else { return }
+        
+        let selectedPost = self.allPosts[indexPath.row]
+        
+        delegate.galleryController(didSelect: selectedPost.image)
     }
 }
